c530f4c2b9ca9be3bb42df4b797b00f0ecb75dad
Vishnu <vishnu@LAPTOP-207GT60Q.localdomain>
2020-08-16 10:10:18 +0530

    Vishnu

--- MailList/src/DAO/UserDAO.java
+++ MailList/src/DAO/UserDAO.java
@@ -13,11 +13,11 @@ public class UserDAO {
        public void saveUser(User user) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
+   
             transaction = session.beginTransaction();
-            // save the student object
+      
             session.save(user);
-            // commit transaction
+    
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -31,11 +31,11 @@ public class UserDAO {
     public void updateUser(User user) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
+  
             transaction = session.beginTransaction();
-            // save the student object
+
             session.update(user);
-            // commit transaction
+ 
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -50,17 +50,17 @@ public class UserDAO {
 
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
+         
             transaction = session.beginTransaction();
 
-            // Delete a user object
+           
             User user = session.get(User.class, id);
             if (user != null) {
                 session.delete(user);
                 System.out.println("user is deleted");
             }
 
-            // commit transaction
+            
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -76,11 +76,11 @@ public class UserDAO {
         Transaction transaction = null;
         User user = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
+          
             transaction = session.beginTransaction();
-            // get an user object
+        
             user = session.get(User.class, id);
-            // commit transaction
+          
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -98,13 +98,13 @@ public class UserDAO {
         Transaction transaction = null;
         List < User > listOfUser = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            // start a transaction
+           
             transaction = session.beginTransaction();
-            // get an user object
+          
 
             listOfUser = session.createQuery("from User").getResultList();
 
-            // commit transaction
+            
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
